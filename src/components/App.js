eb745609eb24c4447a8044714b308c980376d275
alex <alex@guardium.eu>
2019-01-10 13:57:26 +0200

    Added a basic router to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { Router, Route } from 'react-router-dom';
+
+import history from '../history';
 import Header from './Header';
 import Landing from './Landing';
+import PerformersList from './pages/PerformersList';
 
 class App extends React.Component {
     render () {
         return (
             <div className="ui container">
                 <Header />
-                <Landing />
+                <Router history={history}>
+                    <div>
+                        <Route path="/" exact component={Landing} />
+                        <Route path="/performers" exact component={PerformersList} />
+                    </div>
+                </Router>
             </div>
         );
     }
