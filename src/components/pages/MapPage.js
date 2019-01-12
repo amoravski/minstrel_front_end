43429300121be9ce0c3c52ba7082033659c56612
alexmoravski@gmail.com <alexmoravski@gmail.com>
2019-01-12 23:09:09 +0200

    Added leaflet map as a component and a map page

---
+++ src/components/pages/MapPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Sidebar from '../Sidebar';
+import Map from '../Map';
+
+const App = () => {
+    return (
+        <div>
+            <Map />
+            <Sidebar />
+        </div>
+    );
+}
+
+export default App;
