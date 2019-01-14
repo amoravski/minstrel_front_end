6a2ef28157b76dde15f9804873f6d4a6866cd1bd
alexmoravski@gmail.com <alexmoravski@gmail.com>
2019-01-14 21:42:35 +0200

    Added link to landing to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,12 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <div className="ui secondary pointing menu">
-            <h3 className="active item">Minstrel</h3>
+            <Link to="/" className="active item header">Minstrel</Link>
             <div className="right menu">
-                <Link to="/map" className="item">MAP</Link>
-                <Link to="/performers" className="item">PERFORMERS</Link>
-                <Link to="/signin" className="item">SIGN IN</Link>
-                <Link to="/register" className="item">REGISTER</Link>
+                <Link to="/map" className="item">Map</Link>
+                <Link to="/performers" className="item">Performance</Link>
+                <Link to="/signin" className="item">Sign In</Link>
+                <Link to="/register" className="item">Register</Link>
             </div>
         </div>
         );
