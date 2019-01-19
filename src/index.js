5f6fa40c46e4ad10bf4a00f11ad030ea64ada09e
alexmoravski@gmail.com <alexmoravski@gmail.com>
2019-01-19 10:09:38 +0200

    Applied thunk as middleware

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose } from 'redux';
+import reduxThunk from 'redux-thunk';
 
 import reducers from './reducers';
 import App from './components/App';
@@ -9,7 +10,7 @@ import App from './components/App';
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
     reducers,
-    composeEnhancers(applyMiddleware())
+    composeEnhancers(applyMiddleware(reduxThunk))
 );
 
 ReactDOM.render(
