413ad6fd6f0c7112855e6bfe1d8173efb028cb0f
alexmoravski@gmail.com <alexmoravski@gmail.com>
2019-01-19 10:10:33 +0200

    Added a reducer for forms

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
+import registerReducer from './registerReducer';
 
 
 export default combineReducers({
-    form: formReducer
+    form: formReducer,
+    register: registerReducer
 });
