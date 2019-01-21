5406255c28cad709d7a72a1f16827c410486bcff
Alexander <alexmoravski@gmail.com>
2019-01-21 13:44:04 +0200

    Added log in action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,23 @@
 import minstrel_api from '../apis/minstrel_api';
 import history from '../history';
 
-export const registerAdmirer = formValues => async (dispatch, getState) => {
+export const registerAdmirer = formValues => async dispatch => {
     const resp = await minstrel_api.post('/register/admirer', formValues);
 
     dispatch({ type: "CREATE_ADMIRER", payload: resp.data });
     history.push('/');
-}
+};
 
-export const registerPerformer = formValues => async (dispatch, getState) => {
+export const registerPerformer = formValues => async dispatch => {
     const resp = await minstrel_api.post('/register/performer', formValues);
 
     dispatch({ type: "CREATE_PERFORMER", payload: resp.data });
     history.push('/');
-}
+};
+
+export const logIn = formValues => async dispatch => {
+    const resp = await minstrel_api.post('/login', formValues);
+
+    dispatch({ type: "LOG_IN", payload: resp.data });
+    history.push('/');
+};
