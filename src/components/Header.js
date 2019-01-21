6bd6ba50fb4683efce86155e350f4719c8f59caf
Alexander <alexmoravski@gmail.com>
2019-01-21 13:44:49 +0200

    Added log in link to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,8 +8,8 @@ const Header = props => {
             <Link to="/" className={`${props.landing} item`}>Minstrel</Link>
             <div className="right menu">
                 <Link to="/map" className="item">Map</Link>
-                <Link to="/performers" className="item">Performance</Link>
-                <Link to="/signin" className={`${props.signin} item`}>Sign In</Link>
+                <Link to="/performers" className="item">Performers</Link>
+                <Link to="/login" className={`${props.login} item`}>Log In</Link>
                 <Link to="/register" className={`${props.register} item`}>Register</Link>
             </div>
         </div>
