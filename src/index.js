fe4324b5af5f0d7f342cec0ecacecfe561b183db
Alexander <alexmoravski@gmail.com>
2019-01-29 13:26:37 +0200

    Persisted user reducer with redux-persist

--- src/index.js
+++ src/index.js
@@ -2,20 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose } from 'redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 import reduxThunk from 'redux-thunk';
 
 import reducers from './reducers';
 import App from './components/App';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
     reducers,
     composeEnhancers(applyMiddleware(reduxThunk))
 );
 
+const persistor =  persistStore(store)
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+            <App />
+        </PersistGate>
     </Provider>,
     document.querySelector('#root')
 );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,22 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
-import registerReducer from './registerReducer';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
+import registerReducer from './registerReducer';
+import userReducer from './userReducer';
+import loginReducer from './loginReducer';
+
+const persistConfig = {
+    key: 'user',
+    storage
+}
+
+const persistedUser = persistReducer(persistConfig, userReducer)
 
 export default combineReducers({
     form: formReducer,
-    register: registerReducer
+    register: registerReducer,
+    user: persistedUser,
+    login: loginReducer
 });
