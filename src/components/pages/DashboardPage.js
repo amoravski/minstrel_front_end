e4f46ac206e66c9fba3b27ccbb20ad5a7a9a9125
Alexander <alexmoravski@gmail.com>
2019-01-29 13:23:01 +0200

    Added a basic dashboard that only appears to users once logged in

---
+++ src/components/pages/DashboardPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Sidebar from '../Sidebar';
+
+const Dashboard = () => {
+    return (
+        <div>
+            Dashboard
+            <Sidebar />
+        </div>
+    );
+}
+
+export default Dashboard;
