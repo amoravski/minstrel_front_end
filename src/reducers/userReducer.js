de6004ed8c2b9a42f0a146c148df51c94c50a92f
Alexander <alexmoravski@gmail.com>
2019-01-30 22:53:58 +0200

    User type and name now stored as well as jwt

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,7 +1,10 @@
+import jwt_decode from 'jwt-decode';
+
 export default ( state={ signedIn: false }, action ) => {
     switch(action.type) {
         case 'LOG_IN':
-            return { jwt: action.payload.access_token, signedIn: true }; 
+            const decodedJwt = jwt_decode(action.payload.access_token);
+            return { jwt: action.payload.access_token, signedIn: true, name: decodedJwt.identity, type: action.payload.type  }; 
         case 'LOG_OUT':
             return { signedIn: false };
         default:
