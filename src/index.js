6208718be5f5c13ea4f6e7f3029f506888079c56
Alexander <alexmoravski@gmail.com>
2019-01-30 22:52:46 +0200

    Refactored redux store config to separate file

--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware, compose } from 'redux';
-import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react';
-import reduxThunk from 'redux-thunk';
 
-import reducers from './reducers';
+import { store, persistor } from './storeConfig';
 import App from './components/App';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(
-    reducers,
-    composeEnhancers(applyMiddleware(reduxThunk))
-);
-
-const persistor =  persistStore(store)
 ReactDOM.render(
     <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
