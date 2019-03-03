4c28e2e2f9013aa0ccef9077e4c5bc9e870b9359
Alexander <alexmoravski@gmail.com>
2019-03-03 13:04:48 +0200

    Changed LandingSecondServices to LandingThird

--- src/components/pages/LandingPage.js
+++ src/components/pages/LandingPage.js
@@ -2,54 +2,53 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Leaflet from 'leaflet'
 
-import Map from '../map/Map';
 import Header from '../Header';
 import Footer from '../Footer';
-import './LandingPage.css';
 import LandingFirst from '../landing/LandingFirst';
 import LandingSecond from '../landing/LandingSecond';
-import LandingSecondServices from '../landing/LandingSecondServices';
 import LandingThird from '../landing/LandingThird';
+import LandingFourth from '../landing/LandingFourth';
+import DisplayMap from '../map/DisplayMap';
+import './LandingPage.css';
 
 Leaflet.Icon.Default.imagePath =
-'//cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.4/images/'
+    '//cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.4/images/'
 
-class Landing extends React.Component {
-    render () {
+class LandingPage extends React.Component {
+    render() {
         return (
             <div>
-            <div className="ui fluid container">
-                <Header landing="active" />
-                <div className="landing">
-                    <div className="ui computer only grid">
-                        <LandingFirst />
-                        <LandingSecond />
-                        <LandingSecondServices />
-                        <LandingThird />
+                <div className="ui fluid container">
+                    <Header landing="active" />
+                    <div className="landing">
+                        <div className="ui computer only grid">
+                            <LandingFirst />
+                            <LandingSecond />
+                            <LandingThird />
+                            <LandingFourth />
+                        </div>
                     </div>
-                </div>
-                <div>
-                    <div className="ui mobile only grid">
-                        <LandingFirst />
-                        <LandingSecond />
-                        <LandingSecondServices />
-                        <LandingThird />
+                    <div>
+                        <div className="ui mobile only grid">
+                            <LandingFirst />
+                            <LandingSecond />
+                            <LandingThird />
+                            <LandingFourth />
+                        </div>
                     </div>
-                </div>
-                <div style={{ marginBottom: "20vh", width: "80%", marginLeft: "auto", marginRight: "auto", position: "relative"}}>
-                        <div className="circular ui button" style={{ marginBottom: "0vh", zIndex: "20"}}>
+                    <div className="landing-map">
+                        <div className="circular ui button">
                             <Link to="/map">
-                                    Check our map
+                                Check our map
                             </Link>
                         </div>
-                        <Map interact={false} height="80vh" />
-                        
+                        <DisplayMap interact={false} height="80vh" />
+                    </div>
                 </div>
-            </div>
                 <Footer />
             </div>
         );
     }
 }
 
-export default Landing;
+export default LandingPage;
