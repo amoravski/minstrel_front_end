ddd51709b7bfe4f405216c053626581891c6113b
Alexander <alexmoravski@gmail.com>
2019-03-06 23:11:35 +0200

    Added more cases in the performers reducer to handle filter, helper action for filter after getting performers

--- src/reducers/performersReducer.js
+++ src/reducers/performersReducer.js
@@ -1,8 +1,23 @@
+import _ from 'lodash';
+
 export default ( state=[], action ) => {
     switch (action.type){
         case 'GET_PERFORMERS':
-            return action.payload
+            return action.payload;
+        case 'FILTER_PERFORMERS_CATEGORIES':
+            return _.filter(state, function(performer) {
+                return performer.categories
+                    .map(function(category) {
+                    if (category.includes(action.payload)){
+                        return true;
+                    }
+                    return false;
+                    })
+                    .reduce(function(prev, curr) {
+                        return prev || curr;
+                    });
+                });
         default:
-            return state
+            return state;
     }
 }
