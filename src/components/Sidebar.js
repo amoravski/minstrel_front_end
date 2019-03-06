f8a28e37b39e0304a210c2168c114d54a1e8670d
Alexander <alexmoravski@gmail.com>
2019-03-06 23:10:52 +0200

    Added performer search form to sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { openSidebar, closeSidebar, initSidebar } from '../actions';
+import PerformerSearchForm from './forms/PerformerSearchForm';
 import LogOutButton from './LogOutButton';
 
 import './Sidebar.css';
@@ -24,26 +25,31 @@ class Sidebar extends React.Component {
 
     renderSignedOutMenu = aligned => {
         return (
-            <div className={`ui inverted secondary ${aligned} menu`}>
-                <Link to="/" className="item">
-                    <h3>Minstrel</h3>
+            <div>
+                <div className={`ui inverted secondary ${aligned} menu`}>
+                    <Link to="/" className="item">
+                        <h3>Minstrel</h3>
+                    </Link>
+                    <Link to="/map" className={`${this.props.map} item`}>
+                        MAP
                 </Link>
-                <Link to="/map" className={`${this.props.map} item`}>
-                    MAP
+                    <Link to="/performers" className={`${this.props.performers} item`}>
+                        PERFORMERS
                 </Link>
-                <Link to="/performers" className={`${this.props.performers} item`}>
-                    PERFORMERS
+                    <Link to="/login" className="item">
+                        LOG IN
                 </Link>
-                <Link to="/login" className="item">
-                    LOG IN
-                </Link>
-                <Link to="/register" className="item">
-                    SIGN UP
+                    <Link to="/register" className="item">
+                        SIGN UP
                 </Link>
 
-                <button className="close-button ui right floated icon button" onClick={this.toggleSidebar}>
-                    <i className="close icon" ></i>
-                </button>
+                    <button className="close-button ui right floated icon button" onClick={this.toggleSidebar}>
+                        <i className="close icon" ></i>
+                    </button>
+                </div>
+                <div className="performer-search-form">
+                    <PerformerSearchForm />
+                </div>
             </div>
         );
     }
